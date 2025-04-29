294e50d7d32be8b79dfeaa806a4a64e8dbfc7588
Agonz <A.gonzalo_10@hotmail.es>
2025-04-29 21:15:01 +0200

    Geolocalización, rutas a pie,mapa, elegir tipo de transporte y estilos inicio sesion y registro

--- app/src/main/java/com/example/trabajodas3/HomeActivity.java
+++ app/src/main/java/com/example/trabajodas3/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.example.trabajodas3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,5 +9,18 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        findViewById(R.id.btnMapa).setOnClickListener(v ->
+                startActivity(new Intent(HomeActivity.this, MapaActivity.class)));
+
+        findViewById(R.id.btnHorarios).setOnClickListener(v ->
+                startActivity(new Intent(HomeActivity.this, HorariosActivity.class)));
+
+        findViewById(R.id.btnAlarmas).setOnClickListener(v ->
+                startActivity(new Intent(HomeActivity.this, AlarmasActivity.class)));
+
+        findViewById(R.id.btnHistorial).setOnClickListener(v ->
+                startActivity(new Intent(HomeActivity.this, HistorialActivity.class)));
     }
+
 }
